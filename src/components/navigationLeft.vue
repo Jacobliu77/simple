9223f01db0fc500f11babaeef5411413e8cbdc97
e <751232478@qq.com>
2020-06-14 16:51:19 +0800

    对项目的初始化

---
+++ src/components/navigationLeft.vue
@@ -0,0 +1,30 @@
+<!--
+ * @Description: 侧边导航栏模块
+ * @Author: jacob
+ * @Date: 2020-06-14 15:26:48
+ * @LastEditTime: 2020-06-14 15:27:30
+ * @LastEditors: jacob
+--> 
+<template>
+ 
+<div></div>
+ 
+</template>
+ 
+ <script>
+ 
+ export default {
+  data () {
+     return {
+  
+     }  
+   }, 
+   methods: {
+   } 
+ }
+ 
+ </script>
+ 
+<style>
+ 
+</style>
---
+++ src/components/navigationTop.vue
@@ -0,0 +1,30 @@
+<!--
+ * @Description:顶部信息栏导航
+ * @Author: jacob
+ * @Date: 2020-06-14 15:28:56
+ * @LastEditTime: 2020-06-14 15:29:21
+ * @LastEditors: jacob
+--> 
+<template>
+ 
+<div></div>
+ 
+</template>
+ 
+ <script>
+ 
+ export default {
+  data () {
+     return {
+  
+     }  
+   }, 
+   methods: {
+   } 
+ }
+ 
+ </script>
+ 
+<style>
+ 
+</style>
